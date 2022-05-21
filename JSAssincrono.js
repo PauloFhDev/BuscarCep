b5b5ea9bb641a1b15c13b083e93c5ea3b25d516d
Paulo Lima <pauloblfilho87@gmail.com>
2022-05-21 09:58:08 -0300

    first commit

---
+++ JSAssincrono.js
@@ -0,0 +1,14 @@
+let inputElement = document.querySelector('#cep');
+
+//Function pasqisar o cep
+function searchUser(){
+    axios.get(`https://viacep.com.br/ws/${inputElement.value}/json/`).then(res => {
+        document.getElementById('rua').value = res.data.logradouro;
+        document.getElementById('bairro').value = res.data.bairro;
+        document.getElementById('cidade').value = res.data.localidade;
+        document.getElementById('estado').value = res.data.uf;
+    })
+    .catch(() => {
+        alert(`O cep ${inputElement.value} não existe`);
+    })
+}
